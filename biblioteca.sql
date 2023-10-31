f31e3dd74b19040006a9a525a3d450170efd9f58
RCNicolas <nicosandr22@gmail.com>
2023-10-31 10:40:12 -0500

    feat: :card_file_box: Identificacion llaves primarias
    Identificacion de los atributos que seran llaves primarias
    
    @CampusLands

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,59 +1,70 @@
 CREATE TABLE
     infoPago (
-        
+        idInfoPago INT UNIQUE NOT NULL PRIMARY KEY,
+       
     );
 
 CREATE TABLE
     almacenes (
-        
+        codigo INT UNIQUE NOT NULL PRIMARY KEY,
+       
     );
 
 CREATE TABLE
     facturacion (
-       
+        idFactrura INT UNIQUE NOT NULL PRIMARY KEY,
+        
     );
 
 CREATE TABLE
     autoresLibros (
-       
+        
     );
 
 CREATE TABLE
     libros (
+        ISBN INT UNIQUE NOT NULL PRIMARY KEY,
         
     );
 
 CREATE TABLE
     autores (
+        idAutor INT UNIQUE NOT NULL PRIMARY KEY,
        
     );
 
 CREATE TABLE
     stock (
-       
+        idStock INT UNIQUE NOT NULL PRIMARY KEY,
+        
     );
 
 CREATE TABLE
     editorial (
-       
+        idEditorial INT UNIQUE NOT NULL PRIMARY KEY,
+        
     );
 
 CREATE TABLE
     carritos (
-       
+        shoppingCartID INT UNIQUE NOT NULL PRIMARY KEY,
+        
     );
 
 CREATE TABLE
     pedidos (
+        idPedido INT UNIQUE NOT NULL PRIMARY KEY,
         
     );
 
 CREATE TABLE
     clientes (
+        idCliente INT NOT NULL PRIMARY KEY,
         
     );
 
 CREATE TABLE
     notificacionCorreo (
+        idNotificacion INT UNIQUE NOT NULL PRIMARY KEY,
         
     );
