e553d1e8035270c0d7d36373d7deb233ce4bba9c
RCNicolas <nicosandr22@gmail.com>
2023-10-31 09:45:31 -0500

    feat: :card_file_box: Identificacion de entidades
    Identificacion de entidades y creacion de las tablas
    
    @CampusLands

---
+++ biblioteca.sql
@@ -0,0 +1,59 @@
+CREATE TABLE
+    infoPago (
+        
+    );
+
+CREATE TABLE
+    almacenes (
+        
+    );
+
+CREATE TABLE
+    facturacion (
+       
+    );
+
+CREATE TABLE
+    autoresLibros (
+       
+    );
+
+CREATE TABLE
+    libros (
+        
+    );
+
+CREATE TABLE
+    autores (
+       
+    );
+
+CREATE TABLE
+    stock (
+       
+    );
+
+CREATE TABLE
+    editorial (
+       
+    );
+
+CREATE TABLE
+    carritos (
+       
+    );
+
+CREATE TABLE
+    pedidos (
+        
+    );
+
+CREATE TABLE
+    clientes (
+        
+    );
+
+CREATE TABLE
+    notificacionCorreo (
+        
+    );
