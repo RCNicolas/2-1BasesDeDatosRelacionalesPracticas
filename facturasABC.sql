6221df3261f850214a0dd043f881a374607290c8
RCNicolas <nicosandr22@gmail.com>
2023-11-01 07:37:52 -0500

    feat: :card_file_box: Identificacion entidades
    Se identifican las entidades de la base de datos
    
    @CampusLands

---
+++ facturasABC.sql
@@ -0,0 +1,36 @@
+CREATE TABLE pagos(
+    
+);
+CREATE TABLE pedido(
+   
+);
+CREATE TABLE correrosClientes(
+   
+);
+CREATE TABLE telefonosProveedores(
+    
+);
+CREATE TABLE factura(
+    
+);
+CREATE TABLE correosProveedor(
+    
+);
+CREATE TABLE clientes(
+    
+);
+CREATE TABLE articulosFactura(
+   
+);
+CREATE TABLE productos(
+   
+);
+CREATE TABLE PedidiosPendientes(
+    
+);
+CREATE TABLE proveedor(
+   
+);
+CREATE TABLE telefonosClientes(
+    
+);
