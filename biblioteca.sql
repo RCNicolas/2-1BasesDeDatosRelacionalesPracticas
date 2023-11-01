a9529a9b0238dfddb77889b979e78237db6c3865
RCNicolas <nicosandr22@gmail.com>
2023-11-01 06:52:10 -0500

    feat: :card_file_box: Atributos FK
    Se agregan los atributos que posteriormente seran foreing key
    
    @CampusLands

--- biblioteca.sql
+++ biblioteca.sql
@@ -13,18 +13,22 @@ CREATE TABLE
 CREATE TABLE
     facturacion (
         idFactrura INT UNIQUE NOT NULL PRIMARY KEY,
-       
+        idCliente INT NOT NULL,
+        
     );
 
 CREATE TABLE
     autoresLibros (
-        
+        idAutores INT NOT NULL,
+        idLibros INT NOT NULL
     );
 
 CREATE TABLE
     libros (
         ISBN INT UNIQUE NOT NULL PRIMARY KEY,
-       
+        idAutoresLibros INT NOT NULL,
+        idEditorial INT NOT NULL,
+        
     );
 
 CREATE TABLE
@@ -37,36 +41,46 @@ CREATE TABLE
     stock (
         idStock INT UNIQUE NOT NULL PRIMARY KEY,
         
+       
     );
 
 CREATE TABLE
     editorial (
         idEditorial INT UNIQUE NOT NULL PRIMARY KEY,
+        nombre CHAR (20) NOT NULL,
+        idTelefono CHAR (10) NOT NULL,
         
     );
 
 CREATE TABLE
     carritos (
         shoppingCartID INT UNIQUE NOT NULL PRIMARY KEY,
+        ISBN INT NOT NULL,
+        idCliente INT NOT NULL,
         
     );
 
 CREATE TABLE
     pedidos (
         idPedido INT UNIQUE NOT NULL PRIMARY KEY,
+        idInfoPago INT NOT NULL,
+        idFacturacion INT NOT NULL,
         
     );
 
 CREATE TABLE
     clientes (
         idCliente INT NOT NULL PRIMARY KEY,
-        
+        idCorreoElectronico INT NOT NULL,
+        idTelefono CHAR(10) NOT NULL,
+      
     );
 
 CREATE TABLE
     notificacionCorreo (
         idNotificacion INT UNIQUE NOT NULL PRIMARY KEY,
-       
+        idCliente INT NOT NULL,
+        
     );
 
 CREATE TABLE telefonosclientes (
